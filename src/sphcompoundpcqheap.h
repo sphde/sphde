081392623aa178272e9301135e6f5637d78afeef
Steven Munroe <sjmunroe@us.ibm.com>
2016-06-15 14:06:13 -0300

    Add PCQueue support the Compound Heap
    There is a problem in tracking all the block allocations
    while allocating lots of SPHSinglePCQueue_t.  It is easier to use
    Expanding CompoundHeap (fewer and larger blocks to track) while using
    lots of PCQueues.
    
    Signed-off-by:  Steven Munroe  <sjmunroe@us.ibm.com>
    
    	* doc/libsphde-doxygen-sph.doxy (INPUT): Add
    	src/sphcompoundpcqheap.h
    	* doc/sphmaindox.h: Add comment for sphcompoundpcqheap.h.
    	* src/Makefile.am (libsphdeinclude_HEADERS): Add
    	sascompoundpcqheap.h.
    	Add sascompoundheap_t to TESTS.
    	* src/Makefile.in: Regenerate.
    	* src/sphcompoundpcqheap.h: New file.
    	* src/sascompoundheap.cpp: Include sphsinglepcqueue.h and
    	sphcompoundpcqheap.h
    	(SASCompoundHeapExpandInit, SASCompoundHeapInit): Align heap
    	start for page_size > default_page.
    	(SPHCompoundPCQAllocInternal): New function.
    	(SPHCompoundPCQAllocNoLock): New function.
    	(SPHCompoundPCQAlloc): New function.
    	(SPHCompoundPCQFreeInternal): New function.
    	(SPHCompoundPCQFreeNoLock): New function.
    	(SPHCompoundPCQFree): New function.
    	(SPHCompoundPCQNearAllocNoLock): New function.
    	(SPHCompoundPCQNearAlloc): New function.
    	* src/tests/sascompoundheap_t.c: New file.
    	* src/tests/sassim_t.c (sassim_compound_head_test1): Moved
    	to sascompoundheap_t.c, correct spelling of heap.
    	(sassim_compound_head_test2): Moved to
    	sascompoundheap_t.c, correct spelling of heap.
    	* (main): Remove calls to sassim_compound_head_test1 and
    	sassim_compound_head_test2.

---
+++ src/sphcompoundpcqheap.h
@@ -0,0 +1,167 @@
+/*
+ * sphcompoundpcqheap.h
+ *
+ *  Created on: May 28, 2016
+ *      Author: sjmunroe
+ */
+
+#ifndef SRC_SPHCOMPOUNDPCQHEAP_H_
+#define SRC_SPHCOMPOUNDPCQHEAP_H_
+
+
+/*!
+ * \file  src/sphcompoundpcqheap.h
+ * \brief Shared Address Space Compound Heap, PCQueue extension.
+ *
+ * This API extends the basic heap of heaps (CompoundHeap) concept to
+ * allow sub-allocation of collections of other SAS or SPH objects.
+ * In this case specifically a SPHSinglePCQueue_t.
+ *
+ * The base CompoundHeap allocates SAS blocks to be used as a Heap of
+ * other SAS / SPH utility objects, in this case SPHSinglePCQueue_t.
+ * This is a useful construct for managing complex data structures
+ * while maintaining some storage (cache, page, block segment) affinity.
+ * The implementation is based on allocation of sub-heaps and allocating
+ * structures "near" an already allocated object (via NearAlloc).
+ *
+ * With a Compound Heap it is easy to allocate groups of related
+ * SPHSinglePCQueue_t's together from the containing SASCompoundHeap_t
+ * using the SPHCompoundPCQAlloc() API. This API honors the Load Factor
+ * percentage. The Default load factor is 75% (reserving 25% of each
+ * block for near allocation) but can be changed via the
+ * SASCompoundHeapSetLoadFactor() API. A load factor of 100% fills
+ * each block to capacity.
+ *
+ * \note For 100% load factor, there is no difference between Alloc
+ * and NearAlloc.
+ *
+ * Allocatation continues from the current SAS block until
+ * the Load Factor is exceeded for that block. The next allocation
+ * forces the allocation of a new extended heap block and the PCQueue
+ * allocation is satisfied from the new block.
+ *
+ * Any remaining space, above the Load Factor, and any subsequence
+ * freed heap space is available for near allocation.
+ * Subsequent allocations that need good locality to another PCQueue
+ * can use the SPHCompoundPCQNearAlloc() API. This API allocates storage
+ * from the Compound Heap block nearest that provided (near object)
+ * address.
+ *
+ * The allocated PCQueues are always a power of two size allocated
+ * on a matching power of two boundary.
+ * The runtime can always find the containing PCQueue based on the
+ * address of any contained structure. The runtime will allocate from
+ * the immediate containing Simple heap if free space is available there.
+
+ * The runtime can also find the containing Compound Heap for any
+ * PCQueue allocated from it.
+ * This allows a number of extended near allocation schemes.
+ * SAS Compound Heaps can be "expanding" or "fixed".
+ * Finally the storage associated with entire collection of related data
+ * structures allocated from a Compound Heap can be freed for reuse
+ * (destroyed) with one call.
+ *
+ * A Compound Heap and the contained complex data structures can be
+ * arbitrarily large (up the the limits of the Region size or available
+ * disk space). Since SAS blocks are backed by memory mapped files,
+ * contained data structures, can be persistent and larger then
+ * available system memory.
+ *
+ * A new PCQueue can be sub-allocated from a Compound Heap using
+ * ::SPHCompoundPCQAlloc or ::SPHCompoundPCQNearAlloc and freed by
+ * using the functions ::SPHCompoundPCQFree respectively.
+ */
+
+#include "sascompoundheap.h"
+
+#include "sphsinglepcqueue.h"
+
+#ifdef __cplusplus
+#define __C__ "C"
+#else
+#define __C__
+#endif
+
+/*!
+ * \brief Sub-Allocate a new SPH PCQueue from a SAS Compound Heaps
+ * internal space.
+ *
+ * The sas_type_t of \a heap must be SAS_RUNTIME_COMPOUNDHEAP. The allocated
+ * block is initialized as a SPH PCQueue. The function holds a write lock
+ * on the Compound Heap during this operation.
+ *
+ * @param heap Handle to the SASCompoundHeap_t.
+ * @return A newly created SPHSinglePCQueue_t or 0 if an error occurs.
+ */
+extern __C__ SPHSinglePCQueue_t SPHCompoundPCQAlloc (SASCompoundHeap_t heap);
+
+/*!
+ * \brief Free the allocated SPH PCQueue \a block in the SAS Compound
+ * Heap \a heap.
+ *
+ * The sas_type_t of \a heap must be SAS_RUNTIME_COMPOUNDHEAP and the type of
+ * \a free_block must be SAS_RUNTIME_PCQUEUE.
+ *
+ * @param heap Handle to the SASCompoundHeap_t.
+ * @param free_block The created SPHSinglePCQueue_t created from \a heap.
+ */
+extern __C__ void
+SPHCompoundPCQFree (SASCompoundHeap_t heap, SPHSinglePCQueue_t free_block);
+
+/*!
+ * \brief Allocate a new SPH PCQueue from SAS Compound Heap \a nearObj.
+ *
+ * The address \a nearObj is used to find the associated Compound Heap object.
+ * The sas_type_t of the heap must be SAS_RUNTIME_COMPOUNDHEAP. The allocated
+ * SPH PCQueue is already initialized.
+ *
+ * @param nearObj Memory address of SASCompoundHeap_t.
+ * @return A newly created SPHSinglePCQueue_t or 0 if an error occurs.
+ */
+extern __C__ SPHSinglePCQueue_t SPHCompoundPCQNearAlloc (void *nearObj);
+
+/*!
+ * \brief Sub-Allocate a new SPH PCQueue from a SAS Compound Heaps
+ * internal space.
+ *
+ * Similar to ::SASCompoundPCQAlloc but does not take write lock the
+ * Compound Heap.  This API assumes that the application is holding a
+ * write lock on the referenced Compound Heap.  This allows an
+ * application to batch a group of allocations with less overhead.
+ *
+ * @param heap Handle to the SASCompoundHeap_t.
+ * @return A newly created SPHSinglePCQueue_t or 0 if an error occurs.
+ */
+extern __C__ SPHSinglePCQueue_t
+SPHCompoundPCQAllocNoLock (SASCompoundHeap_t heap);
+
+/*!
+ * \brief Allocate a new SPH PCQueue from SAS Compound Heap \a nearObj.
+ *
+ * Similar to ::SASCompoundPCQNearAlloc but does not take write lock the
+ * Compound Heap. This API assumes that the application is holding a
+ * write lock on the referenced Compound Heap.  This allows an
+ * application to batch a group of allocations with less overhead.
+ *
+ * @param nearObj Memory address within a SASCompoundHeap_t.
+ * @return A newly created SPHSinglePCQueue_t or 0 if an error occurs.
+ */
+extern __C__ SPHSinglePCQueue_t SPHCompoundPCQNearAllocNoLock (void *nearObj);
+
+/*!
+ * \brief Free the allocated SPH PCQueue \a free_block from SAS Compound
+ * Heap \a heap.
+ *
+ * Similar to ::SASCompoundPCQFree but does not take write lock the
+ * Compound Heap. This API assumes that the application is holding a
+ * write lock on the referenced Compound Heap.  This allows an
+ * application to batch a group of frees with less overhead.
+ *
+ * @param heap Handle to the SASCompoundHeap_t.
+ * @param free_block The created SPHSinglePCQueue_t created from \a heap.
+ */
+extern __C__ void
+SPHCompoundPCQFreeNoLock (SASCompoundHeap_t heap,
+		SPHSinglePCQueue_t free_block);
+
+#endif /* SRC_SPHCOMPOUNDPCQHEAP_H_ */
